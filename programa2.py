06b33e5ad3144b5354f1c7c6a8072e524b6bade2
Juanita Munar <juanita.munar0.0@gmail.com>
2024-04-10 15:21:44 -0500

    ejemplos

--- programa2.py
+++ programa2.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-a= np.array([5,10,15,20,25])
-#ndarray
-c= a.sum() #suma de los elementos
-c= a.min() #el elemento de menor valor
+a= np.array([[5,10,-1,20,25],[3,6,4,8,12]])
+b= np.array([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(a)
+print(b)
+c= np.add(a,b)
+print(c)
+
---
+++ programap.py
@@ -0,0 +1,4 @@
+import numpy as np
+a= np.array([[5,10,-1,20,25],[3,6,4,8,12]])
+print(a)
+print(a.sum(axis=1))
